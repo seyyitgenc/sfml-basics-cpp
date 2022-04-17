532b82d75d57b49511d9a7adb65b6d8dde90cf9d
seyyitgenc <mahluk053@gmail.com>
2022-04-17 20:48:03 +0300

    Shapes

---
+++ src/Drawing.cpp
@@ -0,0 +1,73 @@
+#include <SFML/Graphics.hpp>
+
+void renderingThread(sf::RenderWindow *window)
+{
+    
+    sf::Texture texture;
+    if (!texture.loadFromFile("tree.png"))
+    {
+        // error
+    }
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setTextureRect(sf::IntRect(0, 0, 48, 32));
+    sprite.setScale(sf::Vector2f(4, 4)); // absolute scale factor
+    // sprite.setOrigin(sf::Vector2f(25.f, 25.f));
+
+    sf::CircleShape circle(60);
+    circle.setFillColor(sf::Color(100, 250, 50));
+    circle.setPosition(100, 200);
+    circle.setOutlineThickness(10);
+    circle.setOutlineColor(sf::Color(100, 100, 100));
+    circle.setTexture(&texture);
+    circle.setTextureRect(sf::IntRect(0, 0, 100, 100));
+    // sprite.setColor(sf::Color(255, 255, 255, 128)); // half transparent
+    sprite.setColor(sf::Color(0, 255, 0)); // green
+
+    sf::CircleShape triangle(80, 3);
+    triangle.setPosition(60, 30);
+    triangle.setFillColor(sf::Color(255, 0, 0));
+    sf::CircleShape square(80, 4);
+    square.setPosition(240, 30);
+    square.setFillColor(sf::Color(255, 0, 0));
+    sf::CircleShape octagon(80, 8);
+    octagon.setPosition(420, 30);
+    octagon.setFillColor(sf::Color(255, 0, 0));
+    // 120x60 rectangle
+    sf::RectangleShape rectangle(sf::Vector2f(120, 60));
+    rectangle.setPosition(200, 400);
+    // convex shapes
+    sf::ConvexShape convex;
+    convex.setPointCount(6);
+    convex.setPoint(0, sf::Vector2f(0, 0));
+    convex.setPoint(1, sf::Vector2f(150, 10));
+    convex.setPoint(2, sf::Vector2f(120, 90));
+    convex.setPoint(3, sf::Vector2f(30, 100));
+    convex.setPoint(4, sf::Vector2f(0, 50));
+    convex.setPosition(0, 400);
+
+    // line with thickness
+    sf::RectangleShape line1(sf::Vector2f(150, 5));
+    line1.rotate(45);
+    sf::Vertex line2[] =
+        {
+            sf::Vertex(sf::Vector2f(10.f, 10.f)),
+            sf::Vertex(sf::Vector2f(150.f, 150.f)),
+        };
+
+    window->setActive(true);
+    while (window->isOpen())
+    {
+        window->clear();
+        window->draw(circle);
+        window->draw(sprite);
+        window->draw(triangle);
+        window->draw(square);
+        window->draw(octagon);
+        window->draw(rectangle);
+        window->draw(convex);
+        window->draw(line2, 2, sf::Lines);
+        // window->draw(line2, 2, sf::Lines);
+        window->display();
+    }
+}
--- src/Window.cpp
+++ src/Window.cpp
@@ -10,43 +10,17 @@ void Window()
     screen.setPosition(sf::Vector2i(10, 10)); // re-position
     // screen.setSize(sf::Vector2u(600, 600));   // re-size
     screen.setTitle("SFML Window"); // re-title
-
-    sf::Texture texture;
-    if (!texture.loadFromFile("tree.png"))
-    {
-        std::cout << "Somethings Wrong" << std::endl;
-    }
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
-    sprite.setTextureRect(sf::IntRect(0, 0, 48, 32));
-    sprite.setScale(sf::Vector2f(4, 4)); // absolute scale factor
-    // sprite.setOrigin(sf::Vector2f(25.f, 25.f));
-
-    sf::CircleShape circle(60);
-    circle.setFillColor(sf::Color(100, 250, 50));
-    circle.setPosition(100, 200);
-    circle.setOutlineThickness(10);
-    circle.setOutlineColor(sf::Color(100, 100, 100));
-    circle.setTexture(&texture);
-    circle.setTextureRect(sf::IntRect(0, 0, 100, 100));
-    // circle.setPointCount(10000);
-
-    // sprite.setColor(sf::Color(255, 255, 255, 128)); // half transparent
-
-    sprite.setColor(sf::Color(0, 255, 0)); // green
-
+    screen.setActive(false);
+    sf::Thread thread(&renderingThread, &screen);
+    thread.launch();
     // screen.setVerticalSyncEnabled(true); //never use both of them at the same time
     screen.setFramerateLimit(60); // it's not much accurate like setVerticalSyncEnabled
     while (screen.isOpen())
     {
         sf::Event event;
         // event loop is MUST
-        screen.setActive(false);
-        sf::Thread thread(&renderingThread, &screen);
-        thread.launch();
         while (screen.pollEvent(event))
         {
-            // "close requested" event: we close the window
             // can be used with switch - case
             if (event.type == sf::Event::Closed)
                 // window close event
